f334f15fc5fc0988fce67a05ea1757c203b4f08b
BLooDBRothER <computertechnology446@gmail.com>
2023-10-17 18:30:08 +0530

    Add e2e tests

--- react/tic-tac-toe-tdd/cypress/e2e/tic-tac-toe.cy.js
+++ react/tic-tac-toe-tdd/cypress/e2e/tic-tac-toe.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
-describe('Just visit e2e test', () => {
-    it('should visit', () => {
+describe('e2e test', () => {
+    it('e2e test for X won', () => {
         cy.visit('/');
 
         cy.get('[data-cy=board]').children().should('have.length', 9);
@@ -16,8 +16,57 @@ describe('Just visit e2e test', () => {
         cy.get('[data-cy=square]').eq(2).click();
 
         cy.get('[data-cy=square]').eq(0).should('have.class', 'bg-white/80 text-black');
+        cy.get('[data-cy=square]').eq(1).should('have.class', 'bg-white/80 text-black');
+        cy.get('[data-cy=square]').eq(2).should('have.class', 'bg-white/80 text-black');
 
         cy.get('[data-cy=winner]').children().should('have.text', 'X Won the match');
+
+    })
+
+    it('e2e test for O won', () => {
+        cy.visit('/');
+
+        cy.get('[data-cy=board]').children().should('have.length', 9);
+        cy.get('[data-cy=turnX]').should('have.class', 'bg-slate-500');
+
+        cy.get('[data-cy=square]').eq(1).click();
+
+        cy.get('[data-cy=turnO]').should('have.class', 'bg-slate-500');
+
+        cy.get('[data-cy=square]').eq(0).click();
+        cy.get('[data-cy=square]').eq(2).click();
+        cy.get('[data-cy=square]').eq(4).click();
+        cy.get('[data-cy=square]').eq(3).click();
+        cy.get('[data-cy=square]').eq(8).click();
+
+        cy.get('[data-cy=square]').eq(0).should('have.class', 'bg-white/80 text-black');
+        cy.get('[data-cy=square]').eq(4).should('have.class', 'bg-white/80 text-black');
+        cy.get('[data-cy=square]').eq(8).should('have.class', 'bg-white/80 text-black');
+
+        cy.get('[data-cy=winner]').children().should('have.text', 'O Won the match');
+
+    })
+
+    it('e2e test for draw', () => {
+        cy.visit('/');
+
+        cy.get('[data-cy=board]').children().should('have.length', 9);
+        cy.get('[data-cy=turnX]').should('have.class', 'bg-slate-500');
+
+        cy.get('[data-cy=square]').eq(0).click();
+
+        cy.get('[data-cy=turnO]').should('have.class', 'bg-slate-500');
+
+        cy.get('[data-cy=square]').eq(1).click();
+        cy.get('[data-cy=square]').eq(2).click();
+        cy.get('[data-cy=square]').eq(4).click();
+        cy.get('[data-cy=square]').eq(3).click();
+        cy.get('[data-cy=square]').eq(5).click();
+        cy.get('[data-cy=square]').eq(7).click();
+        cy.get('[data-cy=square]').eq(6).click();
+        cy.get('[data-cy=square]').eq(8).click();
+
+        cy.get('[data-cy=winner]').children().should('have.text', 'Match Draw');
 
     })
 })
